445bab6fdd7e2b244eb5a7c12d4257281cf8ba4c
Fatma Güney <fguney@ku.edu.tr>
2021-01-13 10:27:55 +0000

    solutions

--- main.sh
+++ main.sh
@@ -1,1 +1,9 @@
-echo Hello World
+alias lread='ls --human-readable --size -1 -S --classify'
+
+# Command for running this in the bg
+sh process.sh &
+
+# Return it to fg
+fg %1
+
+# Then kill => CRTL + C
